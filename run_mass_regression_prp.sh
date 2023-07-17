c6ace9d5dacdc96c533cacebe18bef8f05a355c4
Farouk <farouk.mokhtar@gmail.com>
2023-07-17 17:07:52 +0200

    up

--- run_mass_regression_prp.sh
+++ run_mass_regression_prp.sh
@@ -3,7 +3,7 @@ mkdir -p /fmhwwvol/experiments/mass_regression_test
 python -u weaver/train.py \
  --gpu "0,1" \
  --train-mode regression \
- --batch-size 768 --start-lr 6.75e-3 --num-epochs 100 --optimizer ranger \
+ --batch-size 768 --start-lr 6.75e-3 --num-epochs 2000 --optimizer ranger \
  --data-train '/fmhwwvol/ntuples/*/train/*.root' \
  --data-test '/fmhwwvol/ntuples/*/test/*.root' \
  --data-config weaver/data_new/finetune/FM_ak8_mass_regression_v2.yaml \
