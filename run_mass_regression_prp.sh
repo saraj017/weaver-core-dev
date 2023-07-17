db3cb8f2242fc8acc1b46ad2104496805de676fd
Farouk <farouk.mokhtar@gmail.com>
2023-07-17 17:07:41 +0200

    up

--- run_mass_regression_prp.sh
+++ run_mass_regression_prp.sh
@@ -6,15 +6,15 @@ python -u weaver/train.py \
  --batch-size 768 --start-lr 6.75e-3 --num-epochs 100 --optimizer ranger \
  --data-train '/fmhwwvol/ntuples/*/train/*.root' \
  --data-test '/fmhwwvol/ntuples/*/test/*.root' \
- --data-config weaver/data_new/finetune/FM_ak8_mass_regression.yaml \
+ --data-config weaver/data_new/finetune/FM_ak8_mass_regression_v2.yaml \
  --network-config weaver/networks/fintune_test/mlp_2p_gated_regression.py \
- --model-prefix /fmhwwvol/experiments/mass_regression_custom/model \
- --log-file /fmhwwvol/experiments/mass_regression_custom/logs/train.log \
- --predict-output /fmhwwvol/experiments/mass_regression_custom/predict/pred.root \
+ --model-prefix /fmhwwvol/experiments/mass_regression_custom_v2/model \
+ --log-file /fmhwwvol/experiments/mass_regression_custom_v2/logs/train.log \
+ --predict-output /fmhwwvol/experiments/mass_regression_custom_v2/predict/pred.root \
  --num-workers 0 \
  --in-memory \
  --steps-per-epoch=1 \
- --tensorboard _custom \
+ --tensorboard _custom_v2 \
  --load-model-weights finetune_gghww_custom \
 
 mv runs/* /fmhwwvol/runs/
