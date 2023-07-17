d57df14c148dd488464b1df0d86b1a009256b4e4
Farouk <farouk.mokhtar@gmail.com>
2023-07-17 10:48:01 +0200

    up

--- run_mass_regression.sh
+++ run_mass_regression.sh
@@ -2,12 +2,14 @@ python weaver/train.py \
  --gpus '' \
  --train-mode regression \
  --batch-size 768 --start-lr 6.75e-3 --num-epochs 1 --optimizer ranger \
- --data-train 'datafiles/ntuples/*/out.root' \
- --data-test 'datafiles/ntuples/*/out.root' \
+ --data-train 'datafiles/ntuples/*/*.root' \
+ --data-test 'datafiles/ntuples/*/*.root' \
  --data-config weaver/data_new/finetune/FM_ak8_mass_regression.yaml \
  --network-config weaver/networks/fintune_test/mlp_2p_gated_regression.py \
- --model-prefix experiments/mass_regression/ \
+ --model-prefix experiments/mass_regression/mass_regression \
  --log-file experiments/mass_regression/logs/train.log \
  --predict-output experiments/mass_regression/predict/pred.root \
- --num-workers=0
-#  --tensorboard mass_regression
+ --num-workers=0 \
+ --in-memory \
+ --steps-per-epoch=1 \
+ --tensorboard _farakiko
--- run_mass_regression_prp.sh
+++ run_mass_regression_prp.sh
@@ -1,7 +1,7 @@
 mkdir -p /fmhwwvol/experiments/mass_regression_test
 
 python -u weaver/train.py \
- --gpu "0" \
+ --gpu "0,1" \
  --train-mode regression \
  --batch-size 768 --start-lr 6.75e-3 --num-epochs 100 --optimizer ranger \
  --data-train '/fmhwwvol/ntuples/*/train/*.root' \
