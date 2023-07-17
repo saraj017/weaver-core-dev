bd985b755951d6c0dd81c754dca036f17077ec83
Farouk <farouk.mokhtar@gmail.com>
2023-07-17 11:43:32 +0200

    custom

--- run_mass_regression_prp.sh
+++ run_mass_regression_prp.sh
@@ -8,12 +8,13 @@ python -u weaver/train.py \
  --data-test '/fmhwwvol/ntuples/*/test/*.root' \
  --data-config weaver/data_new/finetune/FM_ak8_mass_regression.yaml \
  --network-config weaver/networks/fintune_test/mlp_2p_gated_regression.py \
- --model-prefix /fmhwwvol/experiments/mass_regression_test/model \
- --log-file /fmhwwvol/experiments/mass_regression_test/logs/train.log \
- --predict-output /fmhwwvol/experiments/mass_regression_test/predict/pred.root \
+ --model-prefix /fmhwwvol/experiments/mass_regression_custom/model \
+ --log-file /fmhwwvol/experiments/mass_regression_custom/logs/train.log \
+ --predict-output /fmhwwvol/experiments/mass_regression_custom/predict/pred.root \
  --num-workers 0 \
  --in-memory \
  --steps-per-epoch=1 \
- --tensorboard _test \
+ --tensorboard _custom \
+ --load-model-weights finetune_gghww_custom \
 
 mv runs/* /fmhwwvol/runs/
