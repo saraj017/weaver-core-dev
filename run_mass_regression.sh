18519154152fa8a619fc5be4f21fb5d28a833992
Farouk <farouk.mokhtar@gmail.com>
2023-07-19 13:07:17 +0200

    up

--- run_mass_regression.sh
+++ run_mass_regression.sh
@@ -1,15 +1,20 @@
-python weaver/train.py \
- --gpus '' \
+#!/bin/bash
+
+python -u weaver/train.py \
+ --gpu "0,1" \
  --train-mode regression \
- --batch-size 768 --start-lr 6.75e-3 --num-epochs 1 --optimizer ranger \
- --data-train 'datafiles/ntuples/*/*.root' \
- --data-test 'datafiles/ntuples/*/*.root' \
- --data-config weaver/data_new/finetune/FM_ak8_mass_regression.yaml \
+ --batch-size 768 --start-lr 6.75e-4 --num-epochs 3000 --optimizer ranger \
+ --data-train '/fmhwwvol/ntuples/*/train/*.root' \
+ --data-test '/fmhwwvol/ntuples/*/test/*.root' \
+ --data-config weaver/data_new/finetune/FM_ak8_mass_regression_v1.yaml \
  --network-config weaver/networks/fintune_test/mlp_2p_gated_regression.py \
- --model-prefix experiments/mass_regression/mass_regression \
- --log-file experiments/mass_regression/logs/train.log \
- --predict-output experiments/mass_regression/predict/pred.root \
- --num-workers=0 \
+ --model-prefix /fmhwwvol/experiments/mass_regression_v1/model \
+ --log-file /fmhwwvol/experiments/mass_regression_v1/logs/train.log \
+ --predict-output /fmhwwvol/experiments/mass_regression_v1/predict/pred.root \
+ --num-workers 0 \
  --in-memory \
  --steps-per-epoch=1 \
- --tensorboard _farakiko
+ --tensorboard _v1 \
+ --load-model-weights finetune_gghww_custom \
+
+mv runs/* /fmhwwvol/runs/
