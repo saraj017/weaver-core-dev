703644d064219b7a24f7935e6f7f8a2051109f2f
Farouk <farouk.mokhtar@gmail.com>
2023-07-26 10:16:45 +0200

    v10

--- run_mass_regression.sh
+++ run_mass_regression.sh
@@ -6,15 +6,15 @@ python -u weaver/train.py \
  --batch-size 768 --start-lr 6.75e-4 --num-epochs 3000 --optimizer ranger \
  --data-train '/fmhwwvol/ntuples/*/train/*.root' \
  --data-test '/fmhwwvol/ntuples/*/test/*.root' \
- --data-config weaver/data_new/finetune/FM_ak8_mass_regression_v2.yaml \
+ --data-config weaver/data_new/finetune/FM_ak8_mass_regression_v10.yaml \
  --network-config weaver/networks/fintune_test/mlp_2p_gated_regression.py \
- --model-prefix /fmhwwvol/experiments/mass_regression_v2/model \
- --log-file /fmhwwvol/experiments/mass_regression_v2/logs/train.log \
- --predict-output /fmhwwvol/experiments/mass_regression_v2/predict/pred.root \
+ --model-prefix /fmhwwvol/experiments/mass_regression_v10/model \
+ --log-file /fmhwwvol/experiments/mass_regression_v10/logs/train.log \
+ --predict-output /fmhwwvol/experiments/mass_regression_v10/predict/pred.root \
  --num-workers 0 \
  --in-memory \
  --steps-per-epoch=1 \
- --tensorboard _v2 \
+ --tensorboard _v10 \
  --load-model-weights finetune_gghww_custom \
 
 mv runs/* /fmhwwvol/runs/
