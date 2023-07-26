52d2611855d9942e88495807a3dc5f89ea699149
Farouk <farouk.mokhtar@gmail.com>
2023-07-26 10:26:58 +0200

    up

--- run_mass_regression.sh
+++ run_mass_regression.sh
@@ -6,7 +6,7 @@ python -u weaver/train.py \
  --batch-size 768 --start-lr 6.75e-4 --num-epochs 3000 --optimizer ranger \
  --data-train '/fmhwwvol/ntuples/*/train/*.root' \
  --data-test '/fmhwwvol/ntuples/*/test/*.root' \
- --data-config weaver/data_new/finetune/mass_regression_v10.yaml \
+ --data-config weaver/data_new/finetune/FM_ak8_mass_regression_v10.yaml \
  --network-config weaver/networks/fintune_test/mlp_2p_gated_regression.py \
  --model-prefix /fmhwwvol/experiments/mass_regression_v10_load/model \
  --log-file /fmhwwvol/experiments/mass_regression_v10_load/logs/train.log \
