3b746ff61732a5a5a077276e63c5452a56b7e2f5
saraj017 <140458900+saraj017@users.noreply.github.com>
2023-07-26 09:02:17 -0700

    Update run_mass_regression_sara.sh

--- run_mass_regression_sara.sh
+++ run_mass_regression_sara.sh
@@ -6,7 +6,7 @@ python -u weaver/train.py \
  --batch-size 768 --start-lr 6.75e-4 --num-epochs 3000 --optimizer ranger \
  --data-train '/fmhwwvol/ntuples/*/train/*.root' \
  --data-test '/fmhwwvol/ntuples/*/test/*.root' \
- --data-config weaver/data_new/finetune/FM_ak8_mass_regression_v1.yaml \
+ --data-config weaver/data_new/finetune/SM_ak8_mass_regression_v1.yaml \
  --network-config weaver/networks/fintune_test/mlp_2p_gated_regression.py \
  --model-prefix /fmhwwvol/experiments_sara/mass_regression_v1/model \
  --log-file /fmhwwvol/experiments_sara/mass_regression_v1/logs/train.log \
